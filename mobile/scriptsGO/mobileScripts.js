a4010936b6ceb5b1aae24022f1e5d7cc2f615bfd
g0nzy <47307516+G0nZy@users.noreply.github.com>
2022-05-05 21:15:50 -0700

    scripts are working

--- mobile/scriptsGO/mobileScripts.js
+++ mobile/scriptsGO/mobileScripts.js
@@ -71,23 +71,23 @@ document.addEventListener('DOMContentLoaded', function(){
 
 
 // SWIPER EFFECT
-// var swiper = new Swiper(".mySwiper", {
-//   effect: "coverflow",
-//   grabCursor: true,
-//   centeredSlides: true,
-//   slidesPerView: "auto",
-//   loop: true,
-//   coverflowEffect: {
-//     rotate: 50,
-//     stretch: 0,
-//     depth: 100,
-//     modifier: 1,
-//     slideShadows: true,
-//   },
-//   pagination: {
-//     el: ".swiper-pagination",
-//   },
-// });
+var swiper = new Swiper(".mySwiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  loop: true,
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
 // END OF SWIPER EFFECT
 
 
